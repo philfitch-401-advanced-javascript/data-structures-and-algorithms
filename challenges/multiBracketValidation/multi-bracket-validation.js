a65e0beab4e813d4d2ca16fff713fcdcc1c2b0a9
Phil Fitch <mostreluctantly@gmail.com>
2019-12-02 14:58:46 -0800

    some tests are passing

--- challenges/multiBracketValidation/multi-bracket-validation.js
+++ challenges/multiBracketValidation/multi-bracket-validation.js
@@ -1,5 +1,6 @@
 
 function multiBracketValidation(input) {
+  console.log(input);
   let brackets = [];
   for(let i = 0; i < input.length; i++) {
     if(input[i] === '(' 
@@ -11,7 +12,18 @@ function multiBracketValidation(input) {
       brackets.push(input[i]);
     }
   }
-  
+  for(let i = 0; i < brackets.length; i++) {
+    if(brackets[i] === '(' && brackets[i + 1] === ')'
+    || brackets[i] === '{' && brackets[i + 1] === '}'
+    || brackets[i] === '[' && brackets[i + 1] === ']') {
+      brackets.splice(i, 2);
+      i = 0;
+    }
+  }
+  if(brackets.length > 0) {
+    return false;
+  }
+  return true;
 }
 
-export default multiBracketValidation;
+module.exports = multiBracketValidation;
